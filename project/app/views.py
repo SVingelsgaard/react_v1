9a636980eba1b49c4dbdd844808c6a279ed8f03d
SVingelsgaard <sigurd@vingelsgaard.no>
2022-04-26 20:39:06 +0200

    x

--- project/app/views.py
+++ project/app/views.py
@@ -3,5 +3,5 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def index(request):
-    #return render(request, 'index.html')
-    return HttpResponse("test")
+    return render(request, 'index.html')
+    #return HttpResponse("test")
